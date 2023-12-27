ee1458d7176e909c9d2ebf777fd8c3fd0342377d
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-12-27 10:39:37 +0530

    chore: added the daisyui navbar, hero and footer

--- app/(saas)/_views/HeroView.tsx
+++ app/(saas)/_views/HeroView.tsx
@@ -5,11 +5,20 @@ import { Button, Hero } from "react-daisyui";
 
 const HeroView = () => {
   return (
-    <div>
-      <Hero>
-        <Button>Button</Button>
-      </Hero>
-    </div>
+    <Hero className="min-h-screen">
+      <Hero.Content className="text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+
+          <Button color="primary">Get Started</Button>
+        </div>
+      </Hero.Content>
+    </Hero>
   );
 };
 
