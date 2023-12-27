ca5a901eaa6720ec0e1c314ca1016a827f7ac5fd
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-12-27 11:20:26 +0530

    added dashboard route

---
+++ app/(saas)/(routes)/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import FooterView from "../../_views/FooterView";
+import NavbarView from "../../_views/NavbarView";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-full flex-col">
+      <NavbarView />
+      <div className="grow">{children}</div>
+      <FooterView />
+    </div>
+  );
+}
